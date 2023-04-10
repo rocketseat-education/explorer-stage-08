f2b211c412ae4ae39f088646b7a4f0cc5b33ffab
Tamires Santos <tfs.tamsantos@gmail.com>
2023-04-10 15:15:12 -0300

    Route Params

--- src/server.js
+++ src/server.js
@@ -2,8 +2,13 @@ const express = require('express')
 
 const app = express()
 
-app.get('/message', (request, response) => {
-  response.send('Hello, world!')
+app.get('/message/:id/:user', (request, response) => {
+  const { id, user } = request.params
+
+  response.send(`
+    Mensagem ID: ${id}.
+    Para o usuário ${user}.  
+  `)
 })
 
 const PORT = 3333
