73f99cfe767e1f27df8eceb2fe943bb12bb9ac87
Tamires Santos <tfs.tamsantos@gmail.com>
2023-04-10 15:07:02 -0300

    Método GET

--- src/server.js
+++ src/server.js
@@ -2,5 +2,9 @@ const express = require('express')
 
 const app = express()
 
+app.get('/message', (request, response) => {
+  response.send('Hello, world!')
+})
+
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
