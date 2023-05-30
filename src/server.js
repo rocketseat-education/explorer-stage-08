f950a68d6e39872438c5bc8e8f090e180059ada1
Tamires Santos <tfs.tamsantos@gmail.com>
2023-05-30 14:09:25 -0300

    Organizando a estrutura do projeto

--- src/server.js
+++ src/server.js
@@ -1,13 +1,11 @@
 const express = require('express')
 
+const routes = require('./routes')
+
 const app = express()
 app.use(express.json())
 
-app.post('/users', (request, response) => {
-  const { name, email, password } = request.body
-
-  response.json({ name, email, password })
-})
+app.use(routes)
 
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
