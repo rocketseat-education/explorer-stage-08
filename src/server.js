03f9bfcf0bff663176aea9f6284893f19e0e1748
Tamires Santos <tfs.tamsantos@gmail.com>
2023-05-30 13:58:39 -0300

    Body Params

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 
 const app = express()
+app.use(express.json())
 
 app.post('/users', (request, response) => {
-  response.send('Você chamou o POST')
+  const { name, email, password } = request.body
+
+  response.json({ name, email, password })
 })
 
 const PORT = 3333
