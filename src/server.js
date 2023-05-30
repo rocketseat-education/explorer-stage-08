b31ad7fff45770d0095b6a67238a3532e17fdbb2
Tamires Santos <tfs.tamsantos@gmail.com>
2023-05-30 13:27:21 -0300

    Query Params

--- src/server.js
+++ src/server.js
@@ -11,5 +11,11 @@ app.get('/message/:id/:user', (request, response) => {
   `)
 })
 
+app.get('/users', (request, response) => {
+  const { page, limit } = request.query
+
+  response.send(`Página: ${page}. Mostrar: ${limit}`)
+})
+
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
