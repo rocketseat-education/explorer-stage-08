4355c4757a43a41ca34b98e6af8b9bb9877858fd
Tamires Santos <tfs.tamsantos@gmail.com>
2023-05-30 13:44:36 -0300

    Método POST

--- src/server.js
+++ src/server.js
@@ -2,19 +2,8 @@ const express = require('express')
 
 const app = express()
 
-app.get('/message/:id/:user', (request, response) => {
-  const { id, user } = request.params
-
-  response.send(`
-    Mensagem ID: ${id}.
-    Nome do usuário: ${user}.  
-  `)
-})
-
-app.get('/users', (request, response) => {
-  const { page, limit } = request.query
-
-  response.send(`Página: ${page}. Mostrar: ${limit}`)
+app.post('/users', (request, response) => {
+  response.send('Você chamou o POST')
 })
 
 const PORT = 3333
