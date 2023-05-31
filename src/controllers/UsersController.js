a95e2f855872d57712e4bd9388227dece9bc1575
Tamires Santos <tfs.tamsantos@gmail.com>
2023-05-31 13:16:17 -0300

    Users Controller

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,11 +1,9 @@
 class UsersController {
-  /**
-   * index - GET para listar vários registros.
-   * show - GET para exibir um registro específico.
-   * create - POST para criar um registro.
-   * update - PUT para atualizar um registro.
-   * delete - DELETE para remover um registro.
-   */
+  create(request, response) {
+    const { name, email, password } = request.body
+
+    response.json({ name, email, password })
+  }
 }
 
 module.exports = UsersController
