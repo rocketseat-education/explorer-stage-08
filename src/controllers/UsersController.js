cf08d9338ae2f410e0cdfcbcffc6c4b4564f7c6b
Tamires Santos <tfs.tamsantos@gmail.com>
2023-07-10 12:52:44 -0300

    Cadastrando usuários

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -13,6 +13,11 @@ class UsersController {
       throw new AppError("Este e-mail já está em uso.")
     }
 
+    await database.run(
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+      [name, email, password]
+    )
+
     return response.status(201).json()
   }
 }
