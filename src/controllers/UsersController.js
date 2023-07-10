ae565f3320cfdccce6d03f2b5cdceca3b2349a57
Tamires Santos <tfs.tamsantos@gmail.com>
2023-07-10 15:10:08 -0300

    Atualizando usuário

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -22,6 +22,38 @@ class UsersController {
 
     return response.status(201).json()
   }
+
+  async update(request, response) {
+     const { name, email } = request.body
+     const { id } = request.params
+
+     const database = await sqliteConnection()
+     const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
+
+     if(!user) {
+      throw new AppError("Usuário não encontrado")
+     }
+
+     const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+     if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este e-mail já está em uso.")
+     }
+
+     user.name = name
+     user.email= email
+
+     await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      updated_at = ?
+      WHERE id = ?`, 
+      [user.name, user.email, new Date(), id]
+    )
+
+    return response.json()
+  }
 }
 
 module.exports = UsersController
