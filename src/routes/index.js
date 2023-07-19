d319d97cd899bdc0c19fbea5de4c0e9d192d4980
Tamires Santos <tfs.tamsantos@gmail.com>
2023-07-19 10:42:12 -0300

    Cadastrando nota

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require('express')
 
 const usersRoutes = require('./users.routes')
+const notesRoutes = require('./notes.routes')
 
 const routes = Router()
 routes.use('/users', usersRoutes)
+routes.use('/notes', notesRoutes)
 
 module.exports = routes
---
+++ src/routes/notes.routes.js
@@ -0,0 +1,12 @@
+const { Router } = require('express')
+
+const NotesController = require('../controllers/NotesController')
+
+const notesRoutes = Router()
+
+const notesController = new NotesController()
+
+notesRoutes.post('/:user_id', notesController.create)
+
+
+module.exports = notesRoutes
