82188b105d3ee27b9cd8e835f238e353e41ed23c
Tamires Santos <tfs.tamsantos@gmail.com>
2023-07-24 10:35:00 -0300

    Listando notas

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -54,6 +54,16 @@ class NotesController {
 
     return response.json()
   }
+
+  async index(request, response) {
+    const { user_id } = request.query
+
+    const notes = await knex("notes")
+    .where({ user_id })
+    .orderBy("title")
+
+    return response.json(notes)
+  }
 }
 
 module.exports = NotesController
