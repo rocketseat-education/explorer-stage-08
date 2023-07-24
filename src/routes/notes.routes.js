ca73fe1ecd0e6ed03169a8f5b018e3873fe1ae1b
Tamires Santos <tfs.tamsantos@gmail.com>
2023-07-24 10:27:00 -0300

    Deletando nota

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -8,6 +8,7 @@ const notesController = new NotesController()
 
 notesRoutes.post('/:user_id', notesController.create)
 notesRoutes.get('/:id', notesController.show)
+notesRoutes.delete('/:id', notesController.delete)
 
 
 module.exports = notesRoutes
