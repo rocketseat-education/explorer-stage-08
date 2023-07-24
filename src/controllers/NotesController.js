605849febcd6469c75bf1f55fafa72daaa093f34
Tamires Santos <tfs.tamsantos@gmail.com>
2023-07-24 10:50:12 -0300

    Filtro WhereIn

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -56,12 +56,22 @@ class NotesController {
   }
 
   async index(request, response) {
-    const { title, user_id } = request.query
+    const { title, user_id, tags } = request.query
 
-    const notes = await knex("notes")
-    .where({ user_id })
-    .whereLike("title", `%${title}%`)
-    .orderBy("title")
+    let notes
+
+    if (tags) {
+      const filterTags = tags.split(',').map(tag => tag.trim())
+
+      notes = await knex("tags")
+        .whereIn("name", filterTags)
+        
+    } else {
+      notes = await knex("notes")
+      .where({ user_id })
+      .whereLike("title", `%${title}%`)
+      .orderBy("title")
+    }
 
     return response.json(notes)
   }
