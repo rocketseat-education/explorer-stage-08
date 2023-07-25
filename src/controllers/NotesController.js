d11908e2a7fae587119bf45385550aa0c399eeeb
Tamires Santos <tfs.tamsantos@gmail.com>
2023-07-25 09:01:00 -0300

    Obtendo tags da nota

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -82,7 +82,17 @@ class NotesController {
       .orderBy("title")
     }
 
-    return response.json(notes)
+    const userTags = await knex("tags").where({ user_id })
+    const notesWhithTags = notes.map(note => {
+      const noteTags = userTags.filter(tag => tag.note_id === note.id)
+
+      return {
+        ...note,
+        tags: noteTags
+      }
+    })
+
+    return response.json(notesWhithTags)
   }
 }
 
